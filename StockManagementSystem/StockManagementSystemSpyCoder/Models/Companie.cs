df01e1faff894060262d09956ab565018f27465b
Shimu <eng.shimu@gmail.com>
2019-02-26 23:46:27 +0600

    first projects ui

---
+++ StockManagementSystem/StockManagementSystemSpyCoder/Models/Companie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystemSpyCoder.Models
+{
+    public class Companie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+    }
+}
